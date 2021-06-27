bbbd4404cedb5a91a92ebd3f8a28610b0a0d2599
Sebastian <sebastian@SEBASTIAN-01.local>
2021-06-27 14:08:11 -0400

    signup

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,8 +1,14 @@
-const bodyParser = require('body-parser');
+//jshint esversion:6
 const express= require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 
 
 const app= express();
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+});
 
 app.listen(2000, function(){
     console.log("Server started on port 2000");
